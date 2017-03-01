b90a60d3a3621434aa4aeb5a61aaa10db2a8cf8a
sam-soltech <sam.langberg@soltech.net>
2017-03-01 08:26:21 -0500

    first commit

---
+++ TestApp/basestyles.js
@@ -0,0 +1,20 @@
+import {
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+  btnText:{
+    color:'#FFF',
+    backgroundColor:'transparent',
+    textAlign: 'center',
+    fontWeight:'600'
+  }
+});
+
+
+export default styles;
